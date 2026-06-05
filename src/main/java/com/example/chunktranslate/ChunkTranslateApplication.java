2d503559da7f69d229865b65842b59ca97033d00
zyoes <2843540362@qq.com>
2026-06-05 19:57:32 +0800

    init

---
+++ src/main/java/com/example/chunktranslate/ChunkTranslateApplication.java
@@ -0,0 +1,13 @@
+package com.example.chunktranslate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChunkTranslateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChunkTranslateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chunktranslate/ChunkTranslateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chunktranslate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChunkTranslateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
